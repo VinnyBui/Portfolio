beaec55c56a6a0c1e3f7b664c4568976f6a726f3
Vinh B <vinh.bui0101@gmail.com>
2025-02-14 20:12:19 -0800

    fixed nav bar error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { MenuIcon, X } from "lucide-react";
-import { Sheet, SheetTrigger, SheetContent, SheetClose } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger, SheetContent, SheetClose, SheetTitle  } from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
@@ -34,18 +34,22 @@ const Navbar = () => {
             side="left" 
             className="fixed top-0 left-0 w-[200px] h-full bg-white shadow-lg border-r"
           >
-            {/* Mobile Menu Links */}
-            <nav className="flex flex-col gap-6 mt-10">
-              {["About", "Projects", "Contact"].map((text, index) => (
-                <SheetClose asChild key={index}>
-                  <a key={index} href="#" className="relative inline-flex flex-col items-center group font-medium">
-                    {text}
-                    <span className="w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full mt-1"></span>
-                  </a>
-                </SheetClose>
-              ))}
-            </nav>
+            <div className="flex flex-col items-start mt-4">
+              <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
+
+              <nav className="flex flex-col gap-6 mt-6">
+                {["About", "Projects", "Contact"].map((text, index) => (
+                  <SheetClose asChild key={index}>
+                    <a key={index} href="#" className="relative inline-flex flex-col items-center group font-medium">
+                      {text}
+                      <span className="w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full mt-1"></span>
+                    </a>
+                  </SheetClose>
+                ))}
+              </nav>
+            </div>
           </SheetContent>
+
         </Sheet>
       </div>
     </header>

9ed3c17e305cf575f6206c866792eb27f3e467a4
Vinh B <vinh.bui0101@gmail.com>
2025-02-14 19:42:00 -0800

    added sheetcontent to access the nav bar in mobile (temp solution)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, X } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent, SheetClose } from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
@@ -11,6 +12,7 @@ const Navbar = () => {
           <p>Software Developer</p>
           <h2 className="font-semibold text-4xl md:text-6xl">Vinh Bui</h2>
         </div>
+
         {/* Desktop Navigation Links */}
         <nav className="hidden md:flex gap-10">
           {["About", "Projects", "Contact"].map((text, index) => (
@@ -20,11 +22,31 @@ const Navbar = () => {
             </a>
           ))}
         </nav>
-        <nav className="md:hidden">
-          <button>
-          <MenuIcon size={40}/>
-          </button>
-        </nav>
+
+        {/* Mobile Menu (Sheet) */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="md:hidden">
+              <MenuIcon size={40} />
+            </button>
+          </SheetTrigger>
+          <SheetContent 
+            side="left" 
+            className="fixed top-0 left-0 w-[200px] h-full bg-white shadow-lg border-r"
+          >
+            {/* Mobile Menu Links */}
+            <nav className="flex flex-col gap-6 mt-10">
+              {["About", "Projects", "Contact"].map((text, index) => (
+                <SheetClose asChild key={index}>
+                  <a key={index} href="#" className="relative inline-flex flex-col items-center group font-medium">
+                    {text}
+                    <span className="w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full mt-1"></span>
+                  </a>
+                </SheetClose>
+              ))}
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );

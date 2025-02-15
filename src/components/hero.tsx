7a85bea31ef6ca57244cbe7686763369043828a7
Vinh B <vinh.bui0101@gmail.com>
2025-02-14 20:50:46 -0800

    added animations and delete unused svg

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,15 @@
+"use client"
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  hidden: { opacity: 0},
+  visible: (delay: number) => ({
+    opacity: 1,
+    transition: {delay, duration: 0.5, ease: "easeOut"},
+  }),
+};
 
 const Hero = () => {
   return (
@@ -13,21 +23,40 @@ const Hero = () => {
             alt="Coffee cup"
             width={300}
             height={300}
+            priority
           />
 
           {/* Overlapping Text */}
           <h1 className="absolute top-[0px] md:top-[16px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-10 font-bold font-boska">
             PORTFOLIO
           </h1>
-          <h1 className="absolute top-[60px] md:top-[126px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]">
+          <motion.h1
+            className="absolute top-[60px] md:top-[126px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            custom={0.3} // Delay by 0.3s
+          >
             PORTFOLIO
-          </h1>
-          <h1 className="absolute top-[120px] md:top-[236px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]">
+          </motion.h1>
+          <motion.h1
+            className="absolute top-[120px] md:top-[236px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            custom={0.6} // Delay by 0.6s
+          >
             PORTFOLIO
-          </h1>
-          <h1 className="absolute top-[180px] md:top-[346px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]">
+          </motion.h1>
+          <motion.h1
+            className="absolute top-[180px] md:top-[346px] right-0 translate-x-1/2 md:translate-x-96 -translate-y-1/2 text-6xl md:text-9xl z-0 text-white font-bold font-boska drop-shadow-[2px_2px_0px_black]"
+            initial="hidden"
+            animate="visible"
+            variants={textVariants}
+            custom={0.9} // Delay by 0.9s
+          >
             PORTFOLIO
-          </h1>
+          </motion.h1>
         </div>
       </div>
     </section>

7a5232538615c2b5848013fb428a9d17f62bfe0f
Vinh B <vinh.bui0101@gmail.com>
2025-02-14 19:33:17 -0800

    added hamburger icon to mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,31 @@
+"use client";
 import React from "react";
+import { MenuIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="top-0 left-0 w-full">
-      <nav className="max-w-screen-xl mx-auto flex justify-between px-8 py-4 mt-20 border-t">
+    <header className="top-0 left-0 w-full px-8 md:px-0">
+      <div className="max-w-screen-xl w-full mx-auto flex justify-between py-4 mt-20 border-t items-center md:items-start relative">
+        {/* Left Section */}
         <div>
           <p>Software Developer</p>
-          <h2 className="  font-semibold text-4xl md:text-6xl">Vinh Bui</h2>
+          <h2 className="font-semibold text-4xl md:text-6xl">Vinh Bui</h2>
         </div>
-
-        {/* Navigation Links */}
-        <div className="flex gap-10 ">
-          {["Home", "Projects", "Contact"].map((text, index) => (
-              <a key={index} href="#" className="relative inline-flex flex-col items-center group">
-                {text}
-                <span className="w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full mt-1"></span>
-              </a>
-            ))}
-        </div>
-      </nav>
+        {/* Desktop Navigation Links */}
+        <nav className="hidden md:flex gap-10">
+          {["About", "Projects", "Contact"].map((text, index) => (
+            <a key={index} href="#" className="relative inline-flex flex-col items-center group">
+              {text}
+              <span className="w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full mt-1"></span>
+            </a>
+          ))}
+        </nav>
+        <nav className="md:hidden">
+          <button>
+          <MenuIcon size={40}/>
+          </button>
+        </nav>
+      </div>
     </header>
   );
 };

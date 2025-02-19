e7dec250ebd8fcfbe707d8358f3ae0d9de727c3b
Vinh B <vinh.bui0101@gmail.com>
2025-02-19 13:38:59 -0800

    created a separate project data file to repeatedly use

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,24 +2,7 @@
 import { useSearchParams } from "next/navigation";
 import { notFound } from "next/navigation";
 import ProjectDetails from "@/components/projectDetails";
-
-const projects = [
-  {
-    id: "1",
-    name: "we-buy-cisco.com",
-    description: "This is my project"
-  },
-  {
-    id: "2",
-    name: "we-buy-cisco.com",
-    description: "This is my project2"
-  },
-  {
-    id: "3",
-    name: "Scripts for Cisco Networks",
-    description: "This is my project3"
-  }
-]
+import { projects } from "@/lib/projectsData";
 
 export default function ProjectsPage() {
   const searchParams = useSearchParams();
--- src/components/projectsDemo.tsx
+++ src/components/projectsDemo.tsx
@@ -1,33 +1,9 @@
 "use client"
-
 import React, { useState } from "react";
 import PaginationSelector from "@/components/pageSelector";
 import { Button } from "./ui/button";
 import Link from "next/link";
-
-const projects = [
-  { 
-    id: 1, 
-    name: "we-buy-cisco.com", 
-    media: "/p2.mp4",
-    type: "video",
-    description: "This is the company's side where only those who are authenticated can have access to deleting, creating, and editing data." 
-  },
-  { 
-    id: 2, 
-    name: "we-buy-cisco.com", 
-    media: "/p1.mp4",
-    type: "video",
-    description: "This is the user interface where customers can view available inventory and add them into a shopping cart. Once added, they can email us the order." 
-  },
-  { 
-    id: 3, 
-    name: "Scripts for Cisco Networks", 
-    media: "/p1.mp4",
-    type: "video",
-    description: "Automation scripts where it will go into the machine's terminal and run various test commands to ensure the machine is in working condition.  " 
-  },
-];
+import { projects } from "@/lib/projectsData";
 
 const ProjectsDemo = () => {
   const [currentPage, setCurrentPage] = useState(1);

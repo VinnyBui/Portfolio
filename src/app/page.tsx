7c90b49327dba63e27c66976fac6421d3898ae15
Vinh B <vinh.bui0101@gmail.com>
2025-02-19 18:25:55 -0800

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/hero";
 import About from "@/components/about";
 import Experience from "@/components/experience";
 import ProjectsDemo from "@/components/projectsDemo";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto">
@@ -11,6 +12,7 @@ export default function Home() {
       <About />
       <Experience />
       <ProjectsDemo />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer id="contact" className="text-left py-10 border-t border-gray-500">
+      <p className="text-sm text-gray-500">
+        © {new Date().getFullYear()} Vinh Bui. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;

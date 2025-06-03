68f6be029ea1151d5051e27daec4eab21da172eb
Vinh B <vinh.bui0101@gmail.com>
2025-06-02 19:13:32 -0700

    added mobile section

--- src/components/sections/projectsScrollSection.tsx
+++ src/components/sections/projectsScrollSection.tsx
@@ -13,29 +13,53 @@ export default function ProjectScroll(){
   const x = useTransform(scrollYProgress, [0, 1], ["20%", "-20%"]);
 
   return (
-    <section ref={TargetRef} className="relative h-[150vh]">
+    <>
+    <section ref={TargetRef} className="relative h-[150vh] hidden md:block">
       <div className="sticky top-0 h-screen flex flex-col items-center overflow-hidden">
-        <h1 className="text-9xl font-bold my-14">PROJECTS</h1>
+        <h1 className="text-4xl md:text-9xl font-bold my-14">PROJECTS</h1>
         <motion.div 
           style={{ x }}
-          className="flex gap-4"
+          className="flex flex-col md:flex-row gap-4 "
         >
-        {projects.map((project) => (
-          <Link href={`/projects/${project.id}`} key={project.id}>
-            <div className="w-[500px] h-[500px] relative overflow-hidden border-2 border-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
-              <div className="relative w-full h-full">
-                <Image 
-                  src={project.image} 
-                  alt={project.name} 
-                  fill 
-                  className="object-cover transition-transform duration-500"
-                />
+          {projects.map((project) => (
+            <Link href={`/projects/${project.id}`} key={project.id}>
+              <div className="w-[300px] h-[300px] md:w-[500px] md:h-[500px] relative overflow-hidden border-2 border-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
+                <div className="relative w-full h-full">
+                  <Image 
+                    src={project.image} 
+                    alt={project.name} 
+                    fill 
+                    className="transition-transform duration-500"
+                  />
+                  <h2 className="text-xl font-bold">{project.name}</h2>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </motion.div>
-    </div>
+            </Link>
+          ))}
+        </motion.div>
+      </div>
     </section>
+    <section className="md:hidden">
+      <div className="flex flex-col items-center overflow-hidden">
+        <h1 className="text-4xl font-bold my-14">PROJECTS</h1>
+        <div className="flex flex-col gap-8 mb-8">
+          {projects.map((project) => (
+            <Link href={`/projects/${project.id}`} key={project.id}>
+              <div className="w-[300px] h-[300px] relative overflow-hidden border-2 border-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-[1.02]">
+                <div className="relative w-full h-full">
+                  <Image 
+                    src={project.image} 
+                    alt={project.name} 
+                    fill 
+                    className="object-cover transition-transform duration-500"
+                  />
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  </>
   )
 }

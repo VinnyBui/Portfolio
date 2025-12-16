e6c80338018533df171b4a2b622cfcd899ab7cf6
VinnyBui <vinh.bui0101@gmail.com>
2025-12-15 21:57:48 -0800

    increase delay

--- src/components/features/ChatWidget.tsx
+++ src/components/features/ChatWidget.tsx
@@ -198,7 +198,7 @@ export default function ChatKitWidget() {
                     <motion.div
                       initial={{ opacity: 1 }}
                       exit={{ opacity: 0 }}
-                      transition={{ duration: 0.4 }}
+                      transition={{ duration: 1 }}
                       className="absolute inset-0 flex flex-col items-center justify-center bg-white z-10"
                     >
                       <div className="relative">
